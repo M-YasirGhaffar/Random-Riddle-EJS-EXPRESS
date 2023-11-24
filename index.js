2c90c370908c35eb95e4a8d10ea4e505d65062e9
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2023-11-25 03:02:28 +0500

    EJS Template fixed

--- index.js
+++ index.js
@@ -11,16 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
   try {
-    // Fetch data from the random riddle API using Axios
     const response = await axios.get('https://riddles-api.vercel.app/random');
-    const riddle = response.data.riddle; // Assuming the API returns a single riddle
-    console.log(riddle)
-    // res.send(riddle)
-    const answer = response.data.answer;
-    console.log(answer)
-    res.send("Riddle: "+riddle + "\n \nAnswer: " + answer)
+    const riddle = response.data;
 
-    // Render the EJS template with the fetched data
     res.render('index', { riddle });
   } catch (error) {
     console.error('Error fetching riddle:', error);
