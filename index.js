d9cf8c5787739844b7f8521db95a57ae57621b75
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2023-11-25 02:54:25 +0500

    API working

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const axios = require('axios');
+const bodyParser = require('body-parser')
+
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
   try {
     // Fetch data from the random riddle API using Axios
     const response = await axios.get('https://riddles-api.vercel.app/random');
-    const riddle = response.data; // Assuming the API returns a single riddle
+    const riddle = response.data.riddle; // Assuming the API returns a single riddle
     console.log(riddle)
+    // res.send(riddle)
+    const answer = response.data.answer;
+    console.log(answer)
+    res.send("Riddle: "+riddle + "\n \nAnswer: " + answer)
 
     // Render the EJS template with the fetched data
     res.render('index', { riddle });
